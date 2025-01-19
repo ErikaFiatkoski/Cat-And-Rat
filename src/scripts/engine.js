0929b2272a916789acf3d650b43718885411789b
ErikaFiatkoski <erika.fiatkoski06@gmail.com>
2025-01-19 18:33:30 -0300

    funcao-escolha-de-jogo-criada

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -4,13 +4,53 @@ const state = {
     ratPoints: document.getElementById("rat-points"),
     nameGame: document.getElementById("name-game"),
     tela: document.getElementById("game-principal"),
+    frameGame: document.getElementById("frame-game"),
+    gameList: document.querySelectorAll(".other-game"),
   },
-  value: {
-    escolha: 0,
+  values: {
+    selectedGame: "jogo-da-velha", //id do jogo selecionado pelo jogador. Deixei um pré-selecionado
   },
 };
 
+function listenerSelectGame() {
+  state.view.gameList.forEach((gameOption) => {
+    gameOption.addEventListener("mousedown", () => {
+      if (gameOption.id === state.values.selectedGame) {
+        alert("Jogo já em andamento! :D");
+      } else {
+        state.values.selectedGame = gameOption;
+        configGame(state.values.selectedGame.id);
+      }
+    });
+  });
+} //Verifica qual o jogo selecionado pelo jogador
+
+function configGame(game) {
+  switch (game) {
+    case "jogo-memoria":
+      state.view.frameGame.src = "./games/jogoDaMemoria.html";
+      break;
+    case "jogo-jokenpo":
+      state.view.frameGame.src = "./games/jokenpo.html";
+      break;
+    case "game03":
+      break;
+    case "game04":
+      break;
+    case "jogo-da-velha":
+      state.view.frameGame.src = "./games/jogoDaVelha.html";
+      break;
+    case "game06":
+      break;
+
+    default:
+      break;
+  }
+}
+
 //Main
-function init() {}
+function init() {
+  listenerSelectGame();
+}
 
 init();
