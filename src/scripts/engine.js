2a21ae0085534b69f6acf7fc8db0972223afe57e
ErikaFiatkoski <erika.fiatkoski06@gmail.com>
2025-01-19 13:11:54 -0300

    ajustes layout

---
+++ src/scripts/engine.js
@@ -0,0 +1,14 @@
+const state = {
+  view: {
+    catPoints: document.getElementById("cat-points"),
+    ratPoints: document.getElementById("rat-points"),
+    nameGame: document.getElementById("name-game"),
+    tela: document.getElementById("game-principal"),
+    botao: document.getElementById("criar-grid"),
+  },
+};
+
+//Main
+function init() {}
+
+init();
